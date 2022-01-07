51500f83432982acc21686b3a296e1f65f976e37
Maria Mazur <maria.a.mazur39@gmail.com>
2022-01-07 16:10:20 +0200

    Добавила проверку ввода пользователя

--- Program.cs
+++ Program.cs
@@ -1,22 +1,78 @@
 ﻿using System;
 
-namespace Lesson3_partt1
+namespace Lesson3_part2
 {
     internal class Program
     {
+       
+        /// <summary>
+        /// Проверяет входящую строку (ввод пользователя)
+        /// </summary>
+        /// <param name="validatedString">Проверяемая строка</param>
+        /// <returns>true - строка верна, или false - строка неверна</returns>
+        static bool UserInputIsValid(string validatedString)
+        {
+            bool result =  !string.IsNullOrEmpty(validatedString)  && !string.IsNullOrWhiteSpace(validatedString);
+            
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            int[,] array2D = new int[4, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            string[,] array2D = new string[5, 2];
 
-            string space = "";
-
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-                    Console.WriteLine(space + array2D[i, j]);
-                    space += " ";
+                    int userNumber = i + 1;
+
+                    string userMessage;
+
+                    if (j == 0)
+                    {
+                        userMessage = $"Введите имя {userNumber}-го пользователя:";
+                    }
+                    else
+                    {
+                        userMessage = $"Введите телефон/email {userNumber}-го пользователя:";
+                    }
+
+                    Console.WriteLine(userMessage);
+
+                    array2D[i, j] = Console.ReadLine();
+
+                    while(!UserInputIsValid(array2D[i, j]))
+                    {
+                        Console.WriteLine("Неправильный ввод!");
+                        Console.WriteLine(userMessage);
+                        array2D[i, j] = Console.ReadLine();
+                    }
                 }
+            }
+
+            Console.WriteLine("____________________________________");
+            Console.WriteLine("Ваш справочник:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                string outputString = "";
+
+                for (int j = 0; j < 2; j++)
+                {
+                    int userNumber = i + 1;
+
+                    if (j == 0)
+                    {
+                        outputString = $"Пользователь {userNumber}:" + array2D[i, j] + ", ";
+                    }
+                    else
+                    {
+                        outputString += array2D[i, j];
+                    }
+                }
+
+                Console.WriteLine(outputString);
             }
 
             Console.ReadKey();
